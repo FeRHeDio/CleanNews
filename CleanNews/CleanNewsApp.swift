3a9474e05e0e13f796142312cb649df8d7625d7c
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-08-11 08:35:29 -0300

    Initial Commit

---
+++ CleanNews/CleanNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanNewsApp.swift
+//  CleanNews
+//
+//  Created by Fernando Putallaz on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CleanNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
