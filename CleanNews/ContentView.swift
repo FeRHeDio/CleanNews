721f36c919b195b51f9666d96799c42f8f5f1ba4
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-08-12 09:02:21 -0300

    Setting the table for this journey with a simple change

--- CleanNews/ContentView.swift
+++ CleanNews/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("News App")
             .padding()
     }
 }
@@ -19,3 +19,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
