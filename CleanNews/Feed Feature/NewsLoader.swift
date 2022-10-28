ea7cc4d8d035e6f0d82184dbb10f5fdff3bc081e
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-10-28 09:55:07 -0300

    Creates NewsLoader boundary and NewsLoaderResult

---
+++ CleanNews/Feed Feature/NewsLoader.swift
@@ -0,0 +1,17 @@
+//
+//  NewsLoader.swift
+//  CleanNews
+//
+//  Created by Fernando Putallaz on 28/10/2022.
+//
+
+import Foundation
+
+enum NewsLoaderResult {
+    case success([NewsItem])
+    case failure(Error)
+}
+
+protocol NewsLoader {
+    func load(completion: @escaping (NewsLoaderResult) -> Void)
+}
