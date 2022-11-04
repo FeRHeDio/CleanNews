f6fe4d2eb14abceb02df3425c7bef6926a29fed3
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-04 07:52:48 -0300

    Save command request cache deletion

--- CleanNewsTests/News Cache/CacheNewsUseCaseTests.swift
+++ CleanNewsTests/News Cache/CacheNewsUseCaseTests.swift
@@ -6,15 +6,26 @@
 //
 
 import XCTest
+import CleanNews
 
 class LocalNewsLoader {
-   init(store: NewsStore) {
-       
+    let store: NewsStore
+    
+    init(store: NewsStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [NewsItem]) {
+        store.deleteCachedNews()
     }
 }
 
 class NewsStore {
     var deleteCachedNewsCallCount = 0
+    
+    func deleteCachedNews() {
+        deleteCachedNewsCallCount += 1
+    }
 }
 
 class CacheNewsUseCaseTests: XCTestCase {
@@ -24,5 +35,21 @@ class CacheNewsUseCaseTests: XCTestCase {
         _ = LocalNewsLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedNewsCallCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let store = NewsStore()
+        let sut = LocalNewsLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedNewsCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func uniqueItem() -> NewsItem {
+        NewsItem(title: "some title", description: "some descri", content: "some content")
     }
 }
