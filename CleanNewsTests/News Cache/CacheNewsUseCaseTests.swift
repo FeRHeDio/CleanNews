5bc06b74a5668271c4a48d52246890dd7f20fe2b
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-05 11:13:30 -0300

    Save command succeeds on successful cache insertion

--- CleanNewsTests/News Cache/CacheNewsUseCaseTests.swift
+++ CleanNewsTests/News Cache/CacheNewsUseCaseTests.swift
@@ -61,7 +61,11 @@ class NewsStore {
     }
     
     func compleInsertion(with error: Error, at index: Int = 0) {
-        deletionCompletions[index](error)
+        insertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
     }
 }
 
@@ -139,6 +143,24 @@ class CacheNewsUseCaseTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         
         XCTAssertEqual(receivedError as NSError?, insertionError)
+    }
+    
+    func test_save_succeedsOnSuccessfulCacheInsertion() {
+        let (sut, store) = makeSUT()
+        let items = [uniqueItem(), uniqueItem()]
+        
+        let exp = expectation(description: "Wait for save completion")
+        var receivedError: Error?
+        sut.save(items) { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccessfully()
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertNil(receivedError)
     }
     
     // MARK: - Helpers
