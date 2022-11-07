1580a87b4f4ff5d8d365a359da7d054b365fc682
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-07 08:15:04 -0300

    Extract cache insertion into a helper function to make logic easier to follow

--- CleanNewsTests/News Cache/CacheNewsUseCaseTests.swift
+++ CleanNewsTests/News Cache/CacheNewsUseCaseTests.swift
@@ -24,11 +24,16 @@ class LocalNewsLoader {
             if let cacheDeletionError = error {
                 completion(cacheDeletionError)
             } else {
-                self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
-                    guard self != nil else { return }
-                    completion(error)
-                }
+                self.cache(items, with: completion)
             }
+        }
+    }
+    
+    private func cache(_ items: [NewsItem], with completion: @escaping (Error?) -> Void ) {
+        store.insert(items, timestamp: currentDate()) { [weak self] error in
+            guard self != nil else { return }
+            
+            completion(error)
         }
     }
 }
