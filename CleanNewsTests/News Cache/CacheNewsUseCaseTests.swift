21c39c15f909f17e0b981b5f49245d67254fc7de
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-07 08:12:41 -0300

    Invert if logic to make code paths easier to follow

--- CleanNewsTests/News Cache/CacheNewsUseCaseTests.swift
+++ CleanNewsTests/News Cache/CacheNewsUseCaseTests.swift
@@ -21,17 +21,18 @@ class LocalNewsLoader {
         store.deleteCachedNews { [weak self] error in
             guard let self = self else { return }
             
-            if error == nil {
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
                 self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
                     guard self != nil else { return }
                     completion(error)
                 }
-            } else {
-                completion(error)
             }
         }
     }
 }
+
 
 protocol NewsStore {
     typealias DeletionCompletion = (Error?) -> Void
