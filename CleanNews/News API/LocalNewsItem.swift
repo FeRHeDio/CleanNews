6a42ebf15116284f9247530a4cac7efe1b5c93a1
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-08 08:21:25 -0300

    Move LocalNewsItem to its own file

---
+++ CleanNews/News API/LocalNewsItem.swift
@@ -0,0 +1,20 @@
+//
+//  LocalNewsItem.swift
+//  CleanNews
+//
+//  Created by Fernando Putallaz on 08/11/2022.
+//
+
+import Foundation
+
+public struct LocalNewsItem: Equatable, Decodable {
+    public let title: String
+    public let description: String
+    public let content: String
+    
+    public init(title: String, description: String, content: String) {
+        self.title = title
+        self.description = description
+        self.content = content
+    }
+}
