74d25c3cd9f90a3250b7aef6bcb25b63b15f6fbf
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-17 17:15:22 -0300

    Extract NewsCacheSpy into shared scope to remove duplication

--- CleanNewsTests/News Cache/CacheNewsUseCaseTests.swift
+++ CleanNewsTests/News Cache/CacheNewsUseCaseTests.swift
@@ -126,44 +126,6 @@ class CacheNewsUseCaseTests: XCTestCase {
         XCTAssertEqual(receivedError as NSError?, expectedError, file: file, line: line)
     }
     
-    private class NewsStoreSpy: NewsStore {
-        enum ReceivedMessage: Equatable {
-            case deleteCachedNews
-            case insert([LocalNewsItem], Date)
-        }
-        
-        private(set) var receivedMessages = [ReceivedMessage]()
-        
-        private var deletionCompletions = [DeletionCompletion]()
-        private var insertionCompletions = [InsertionCompletion]()
-        
-        func deleteCachedNews(completion: @escaping DeletionCompletion) {
-            deletionCompletions.append(completion)
-            receivedMessages.append(.deleteCachedNews)
-        }
-        
-        func completeDeletion(with error: Error, at index: Int = 0) {
-            deletionCompletions[index](error)
-        }
-        
-        func completeDeletionSuccessfully(at index: Int = 0) {
-            deletionCompletions[index](nil)
-        }
-        
-        func insert(_ items: [LocalNewsItem], timestamp: Date, completion: @escaping InsertionCompletion) {
-            insertionCompletions.append(completion)
-            receivedMessages.append(.insert(items, timestamp))
-        }
-        
-        func compleInsertion(with error: Error, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
-        
-        func completeInsertionSuccessfully(at index: Int = 0) {
-            insertionCompletions[index](nil)
-        }
-    }
-    
     private func uniqueItem() -> NewsItem {
         NewsItem(title: "some title", description: "some descri", content: "some content")
     }
