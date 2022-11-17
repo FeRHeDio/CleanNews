e6280580e74cb644d39665d231f7a7e55fbbd13e
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-17 17:12:37 -0300

    LocalNewsLoader does not message store upon creation

---
+++ CleanNewsTests/News Cache/LoadNewsFromCacheUseCaseTests.swift
@@ -0,0 +1,69 @@
+//
+//  LoadNewsFromCacheUseCaseTests.swift
+//  CleanNewsTests
+//
+//  Created by Fernando Putallaz on 17/11/2022.
+//
+
+import XCTest
+import CleanNews
+
+class LoadNewsFromCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalNewsLoader, store: NewsStoreSpy) {
+        let store = NewsStoreSpy()
+        let sut = LocalNewsLoader(store: store, currentDate: currentDate)
+        
+        checkForMemoryLeaks(store, file: file, line: line)
+        checkForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, store)
+    }
+    
+    private class NewsStoreSpy: NewsStore {
+        enum ReceivedMessage: Equatable {
+            case deleteCachedNews
+            case insert([LocalNewsItem], Date)
+        }
+        
+        private(set) var receivedMessages = [ReceivedMessage]()
+        
+        private var deletionCompletions = [DeletionCompletion]()
+        private var insertionCompletions = [InsertionCompletion]()
+        
+        func deleteCachedNews(completion: @escaping DeletionCompletion) {
+            deletionCompletions.append(completion)
+            receivedMessages.append(.deleteCachedNews)
+        }
+        
+        func completeDeletion(with error: Error, at index: Int = 0) {
+            deletionCompletions[index](error)
+        }
+        
+        func completeDeletionSuccessfully(at index: Int = 0) {
+            deletionCompletions[index](nil)
+        }
+        
+        func insert(_ items: [LocalNewsItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+            insertionCompletions.append(completion)
+            receivedMessages.append(.insert(items, timestamp))
+        }
+        
+        func compleInsertion(with error: Error, at index: Int = 0) {
+            insertionCompletions[index](error)
+        }
+        
+        func completeInsertionSuccessfully(at index: Int = 0) {
+            insertionCompletions[index](nil)
+        }
+    }
+
+}
