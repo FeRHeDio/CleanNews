cb98d3a3269704d0090aaf0b0a6b82584941837c
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-17 20:46:44 -0300

    Extract local members into properties

--- CleanNews/News Cache/LocalNewsLoader.swift
+++ CleanNews/News Cache/LocalNewsLoader.swift
@@ -10,6 +10,7 @@ import Foundation
 public final class LocalNewsLoader {
     private let store: NewsStore
     private let currentDate: () -> Date
+    private let calendar = Calendar(identifier: .gregorian)
     
     public typealias SaveResult = Error?
     public typealias LoadResult = NewsLoaderResult
@@ -46,9 +47,12 @@ public final class LocalNewsLoader {
         }
     }
     
+    private var maxCacheDateInDays: Int {
+        return 7
+    }
+    
     private func validate(_ timestamp: Date) -> Bool {
-        let calendar = Calendar(identifier: .gregorian)
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheDateInDays, to: timestamp) else {
             return false
         }
         
