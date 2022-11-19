f38e690963edc6636a3f76a63c9e8430f07f5b86
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-19 11:08:09 -0300

    LocalNewsLoader does not message store upon creation (before validating the cached news)

---
+++ CleanNewsTests/News Cache/ValidateNewsCacheUseCaseTests.swift
@@ -0,0 +1,31 @@
+//
+//  ValidateNewsCacheUseCaseTests.swift
+//  CleanNewsTests
+//
+//  Created by Fernando Putallaz on 19/11/2022.
+//
+
+import XCTest
+import CleanNews
+
+final class ValidateNewsCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalNewsLoader, store: NewsStoreSpy) {
+        let store = NewsStoreSpy()
+        let sut = LocalNewsLoader(store: store, currentDate: currentDate)
+        
+        checkForMemoryLeaks(store, file: file, line: line)
+        checkForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, store)
+    }
+    
+}
