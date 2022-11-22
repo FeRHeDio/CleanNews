138b45535b943055adea14607f1da4b4f430c234
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-21 22:59:25 -0300

    Make the feedCache policy a pure type with no side effects (deterministic)

--- CleanNews/News Cache/LocalNewsLoader.swift
+++ CleanNews/News Cache/LocalNewsLoader.swift
@@ -8,34 +8,29 @@
 import Foundation
 
 public final class NewsCachePolicy {
-    private let currentDate: () -> Date
     private let calendar = Calendar(identifier: .gregorian)
-    
-    init(currentDate: @escaping () -> Date) {
-        self.currentDate = currentDate
-    }
+
     private var maxCacheDateInDays: Int {
         return 7
     }
     
-    func validate(_ timestamp: Date) -> Bool {
+    func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheDateInDays, to: timestamp) else {
             return false
         }
         
-        return currentDate() < maxCacheAge
+        return date < maxCacheAge
     }
 }
 
 public final class LocalNewsLoader {
     private let store: NewsStore
     private let currentDate: () -> Date
-    private let cachePolicy: NewsCachePolicy
+    private let cachePolicy = NewsCachePolicy()
     
     public init(store: NewsStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
-        self.cachePolicy = NewsCachePolicy(currentDate: currentDate)
     }
 }
  
@@ -74,7 +69,7 @@ extension LocalNewsLoader: NewsLoader {
             case let .failure(error):
                 completion(.failure(error))
                 
-            case let .found(news, timestamp) where self.cachePolicy.validate(timestamp):
+            case let .found(news, timestamp) where self.cachePolicy.validate(timestamp, against: self.currentDate()):
                 completion(.success(news.toModels()))
                 
             case .found, .empty:
@@ -95,7 +90,7 @@ extension LocalNewsLoader {
                     print(error)
                 }
                 
-            case let .found(_, timestamp) where !self.cachePolicy.validate(timestamp):
+            case let .found(_, timestamp) where !self.cachePolicy.validate(timestamp, against: self.currentDate()):
                 self.store.deleteCachedNews { _ in }
                 
             case .empty, .found: break
