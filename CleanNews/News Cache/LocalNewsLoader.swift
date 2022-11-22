f809c2db14a75abe23f242edc24d4f516a247578
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-21 23:11:27 -0300

    Move feedCache policy to its own file

--- CleanNews/News Cache/LocalNewsLoader.swift
+++ CleanNews/News Cache/LocalNewsLoader.swift
@@ -7,23 +7,6 @@
 
 import Foundation
 
-public final class NewsCachePolicy {
-    private init() {}
-    
-    private static let calendar = Calendar(identifier: .gregorian)
-    private static var maxCacheDateInDays: Int {
-        return 7
-    }
-    
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheDateInDays, to: timestamp) else {
-            return false
-        }
-        
-        return date < maxCacheAge
-    }
-}
-
 public final class LocalNewsLoader {
     private let store: NewsStore
     private let currentDate: () -> Date
