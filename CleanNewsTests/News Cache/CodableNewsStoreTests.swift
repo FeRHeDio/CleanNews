2f08d22f4b5d795b855258ee4ce6aa1ed6b71f56
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-23 14:48:51 -0300

    Make NewsStoreSpecs public and make test CodableNewsStoreTests conform to the FailableNewsStore

--- CleanNewsTests/News Cache/CodableNewsStoreTests.swift
+++ CleanNewsTests/News Cache/CodableNewsStoreTests.swift
@@ -8,42 +8,7 @@
 import XCTest
 import CleanNews
 
-protocol NewsStoreSpecs {
-    func test_retrieve_deliversEmptyOnEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnEmptyCache()
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
-    
-    func test_insert_deliversNoErrorOnEmptyCache()
-    func test_insert_deliversNoErrorOnNonEmptyCache()
-    func test_insert_overridesPreviouslyInsertedCacheValues()
-    
-    func test_delete_deliversNoErrorOnEmptyCache()
-    func test_delete_hasNoSideEffectOnEmptyCache()
-    func test_delete_deliversNoErrorOnNonEmptyCache()
-    func test_delete_emptiesPreviouslyInsertedCache()
-    
-    func test_storeSideEffects_runSerially()
-}
-
-protocol FailableRetrieveNewsStoreSpecs {
-    func test_retrieve_deliversFailureOnRetrievalError()
-    func test_retrieve_hasNoSideEffectOnFailure()
-}
-
-protocol FailableInsertNewsStoreSpecs {
-    func test_insert_deliversErrorOnInsertionError()
-    func test_insert_hasNoSideEffectsOnInsertionError()
-}
-
-protocol FailableDeleteNewsStoreSpecs {
-    func test_delete_deliversErrorOnDeletionError()
-    func test_delete_hasNoSideEffectsOnDeletionError()
-}
-
-typealias FailableNewsStoreSpecs = FailableRetrieveNewsStoreSpecs & FailableInsertNewsStoreSpecs & FailableDeleteNewsStoreSpecs
-
-final class CodableNewsStoreTests: XCTestCase {
+final class CodableNewsStoreTests: XCTestCase, FailableNewsStore {
     override func setUp() {
         super.setUp()
         
