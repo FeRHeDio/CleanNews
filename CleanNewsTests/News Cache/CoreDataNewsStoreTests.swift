ce8b1e4f85dceccdc86ae24e4e04221db57860f1
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-11-24 15:45:08 -0300

    Create CoreDataNewsStoreTests and conforms to NewsStoreSpecs

---
+++ CleanNewsTests/News Cache/CoreDataNewsStoreTests.swift
@@ -0,0 +1,62 @@
+//
+//  CoreDataNewsStoreTests.swift
+//  CleanNewsTests
+//
+//  Created by Fernando Putallaz on 24/11/2022.
+//
+
+import XCTest
+
+
+class CoreDataNewsStoreTests: NewsStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+            
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+    
+    //MARK: - Helpers
+    
+}
