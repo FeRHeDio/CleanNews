873f2e022027a57f17a1d530fcb7d30ac1d6c329
Fernando Putallaz <ferhedio@gmail.com>
2022-12-27 07:27:24 -0300

    Replace custom HTTPClientResult with Standard Swift Result type.

--- CleanNewsTests/News API/RemoteNewsLoaderTests.swift
+++ CleanNewsTests/News API/RemoteNewsLoaderTests.swift
@@ -186,9 +186,9 @@ final class RemoteNewsLoaderTests: XCTestCase {
             messages.map { $0.url }
         }
         
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
         
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             messages.append((url, completion))
         }
         
@@ -203,7 +203,7 @@ final class RemoteNewsLoaderTests: XCTestCase {
                 httpVersion: nil,
                 headerFields: nil
             )!
-            messages[index].completion(.success(data, response))
+            messages[index].completion(.success((data, response)))
         }
     }
 }
