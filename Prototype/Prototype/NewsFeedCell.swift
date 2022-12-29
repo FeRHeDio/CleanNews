c98e3d8526e9f81cad2bb6f4325bd83736d72756
Fernando Putallaz <ferhedio@gmail.com>
2022-12-29 13:21:42 -0300

    Add NewsFeedCell with proper connections

---
+++ Prototype/Prototype/NewsFeedCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsFeedCell.swift
+//  Prototype
+//
+//  Created by Fernando Putallaz on 29/12/2022.
+//
+
+import UIKit
+
+final class NewsFeedCell: UITableViewCell {
+    @IBOutlet private(set) var titleLabel: UILabel!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+    @IBOutlet private(set) var newsImage: UIImageView!
+}
