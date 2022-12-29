717cb8e6051a437b91fee62f07732a3ca995fc84
Fernando Putallaz <ferhedio@gmail.com>
2022-12-29 10:54:55 -0300

    Add NewsFeedViewController displaying 10 cells

--- Prototype/Prototype/NewsFeedViewController.swift
+++ Prototype/Prototype/NewsFeedViewController.swift
@@ -5,4 +5,14 @@
 //  Created by Fernando Putallaz on 29/12/2022.
 //
 
-import Foundation
+import UIKit
+
+final class NewsFeedViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "NewsFeedCell")!
+    }
+}
