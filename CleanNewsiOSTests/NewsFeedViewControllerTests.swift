3d3c7f06a96eaf8b685b1692c50baa448b39bcf3
Fernando Putallaz <ferhedio@gmail.com>
2023-01-02 11:30:07 -0300

    Loads NewsFeed on viewDidLoad

--- CleanNewsiOSTests/NewsFeedViewControllerTests.swift
+++ CleanNewsiOSTests/NewsFeedViewControllerTests.swift
@@ -6,11 +6,22 @@
 //
 
 import XCTest
+import UIKit
 
-final class NewsFeedViewController {
-    init(loader: NewsFeedViewControllerTests.LoaderSpy) {
-        
+final class NewsFeedViewController: UIViewController {
+    private var loader: NewsFeedViewControllerTests.LoaderSpy?
+    
+    convenience init(loader: NewsFeedViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loader?.load()
+    }
+    
 }
 
 final class NewsFeedViewControllerTests: XCTestCase {
@@ -22,9 +33,22 @@ final class NewsFeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
+    func test_viewDidLoad_loadsNewsFeed() {
+        let loader = LoaderSpy()
+        let sut = NewsFeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
     //MARK: - Helpers
     
     class LoaderSpy {
         private(set) var loadCallCount: Int = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
 }
