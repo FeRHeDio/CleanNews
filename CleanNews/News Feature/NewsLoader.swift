721d7a123d3774d91e7921b25071bcf060bd1e8e
Fernando Putallaz <ferhedio@gmail.com>
2023-03-06 18:04:50 -0300

    Commented NewsFeedViewControllerTests in order to make room for the new framework.

--- CleanNews/News Feature/NewsLoader.swift
+++ CleanNews/News Feature/NewsLoader.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-
-
 public protocol NewsLoader {
     typealias Result = Swift.Result<[NewsItem], Error>
     
--- CleanNewsiOSTests/NewsFeedViewControllerTests.swift
+++ CleanNewsiOSTests/NewsFeedViewControllerTests.swift
@@ -7,56 +7,48 @@
 
 import XCTest
 import UIKit
+//import CleanNews
 
 final class NewsFeedViewController: UIViewController {
-    private var loader: NewsFeedViewControllerTests.LoaderSpy?
-    
-    convenience init(loader: NewsFeedViewControllerTests.LoaderSpy) {
-        self.init()
-        self.loader = loader
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        loader?.load()
-    }
-}
-
-final class NewsFeedViewControllerTests: XCTestCase {
-    
-    func test_init_doesNotLoadFeed() {
-        let loader = LoaderSpy()
-        _ = NewsFeedViewController(loader: loader)
-        
-        XCTAssertEqual(loader.loadCallCount, 0)
-    }
-    
-    func test_viewDidLoad_loadsFeed() {
-        let loader = LoaderSpy()
-        let sut = NewsFeedViewController(loader: loader)
-        
-        sut.loadViewIfNeeded()
-        
-        XCTAssertEqual(loader.loadCallCount, 1)
-    }
-    
-        // MARK: - Helpers
-    
-    class LoaderSpy {
-        private(set) var loadCallCount: Int = 0
-        
-        func load() {
-            loadCallCount += 1
-        }
-    }
-    
-    
-    func testPerformanceExample() throws {
-            // This is an example of a performance test case.
-        self.measure {
-                // Put the code you want to measure the time of here.
-        }
-    }
-    
+//    private var loader: NewsLoader?
+//
+//    convenience init(loader: NewsLoader) {
+//        self.init()
+//        self.loader = loader
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        loader?.load { _ in }
+//    }
+//}
+//
+//final class NewsFeedViewControllerTests: XCTestCase {
+//
+//    func test_init_doesNotLoadFeed() {
+//        let loader = LoaderSpy()
+//        _ = NewsFeedViewController(loader: loader)
+//
+//        XCTAssertEqual(loader.loadCallCount, 0)
+//    }
+//
+//    func test_viewDidLoad_loadsFeed() {
+//        let loader = LoaderSpy()
+//        let sut = NewsFeedViewController(loader: loader)
+//
+//        sut.loadViewIfNeeded()
+//
+//        XCTAssertEqual(loader.loadCallCount, 1)
+//    }
+//
+//        // MARK: - Helpers
+//
+//    class LoaderSpy: NewsLoader {
+//        func load(completion: @escaping (NewsLoader.Result) -> Void) {
+//            loadCallCount += 1
+//        }
+//
+//        private(set) var loadCallCount: Int = 0
+//    }
 }
