70a4c823d1048e40d4c2bdd45f72a74f7c908d63
Fernando Putallaz <ferhedio@gmail.com>
2023-03-07 12:23:07 -0300

    Fix CleanNews Project + deleting Workspace + adding CleanNewsiOS Framework.

--- CleanNewsiOSTests/NewsFeedViewControllerTests.swift
+++ CleanNewsiOSTests/NewsFeedViewControllerTests.swift
@@ -2,53 +2,53 @@
 //  NewsFeedViewControllerTests.swift
 //  CleanNewsiOSTests
 //
-//  Created by Fernando Putallaz on 04/03/2023.
+//  Created by Fernando Putallaz on 07/03/2023.
 //
 
 import XCTest
 import UIKit
-//import CleanNews
+import CleanNewsFramework
 
 final class NewsFeedViewController: UIViewController {
-//    private var loader: NewsLoader?
-//
-//    convenience init(loader: NewsLoader) {
-//        self.init()
-//        self.loader = loader
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        loader?.load { _ in }
-//    }
-//}
-//
-//final class NewsFeedViewControllerTests: XCTestCase {
-//
-//    func test_init_doesNotLoadFeed() {
-//        let loader = LoaderSpy()
-//        _ = NewsFeedViewController(loader: loader)
-//
-//        XCTAssertEqual(loader.loadCallCount, 0)
-//    }
-//
-//    func test_viewDidLoad_loadsFeed() {
-//        let loader = LoaderSpy()
-//        let sut = NewsFeedViewController(loader: loader)
-//
-//        sut.loadViewIfNeeded()
-//
-//        XCTAssertEqual(loader.loadCallCount, 1)
-//    }
-//
-//        // MARK: - Helpers
-//
-//    class LoaderSpy: NewsLoader {
-//        func load(completion: @escaping (NewsLoader.Result) -> Void) {
-//            loadCallCount += 1
-//        }
-//
-//        private(set) var loadCallCount: Int = 0
-//    }
+    private var loader: NewsLoader?
+
+    convenience init(loader: NewsLoader) {
+        self.init()
+        self.loader = loader
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loader?.load { _ in }
+    }
+}
+
+final class NewsFeedViewControllerTests: XCTestCase {
+
+    func test_init_doesNotLoadFeed() {
+        let loader = LoaderSpy()
+        _ = NewsFeedViewController(loader: loader)
+
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = NewsFeedViewController(loader: loader)
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+
+        // MARK: - Helpers
+
+    class LoaderSpy: NewsLoader {
+        func load(completion: @escaping (NewsLoader.Result) -> Void) {
+            loadCallCount += 1
+        }
+
+        private(set) var loadCallCount: Int = 0
+    }
 }
