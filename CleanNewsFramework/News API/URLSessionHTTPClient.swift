6bee537fae287567733d67e174eaeec096aea60b
Fernando Putallaz <ferhedio@gmail.com>
2023-03-28 18:35:44 -0300

    Add `HTTPClientTask` so clients can cancel HTTP requests.

--- CleanNewsFramework/News API/URLSessionHTTPClient.swift
+++ CleanNewsFramework/News API/URLSessionHTTPClient.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public class URLSessionHTTPClient: HTTPClient {
+public final class URLSessionHTTPClient: HTTPClient {
     private var session: URLSession
     
     public init(session: URLSession = .shared) {
@@ -16,10 +16,18 @@ public class URLSessionHTTPClient: HTTPClient {
     
     private struct UnexpectedValueRepresentation: Error {}
     
-    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void ) {
-        session.dataTask(with: url) { data, response, error in
+    private struct URLSessionTaskWrapper: HTTPClientTask {
+        let wrapped: URLSessionTask
+        
+        func cancel() {
+            wrapped.cancel()
+        }
+    }
+    
+    
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        let task = session.dataTask(with: url) { data, response, error in
             completion(Result {
-                
                 if let error = error {
                     throw error
                 } else if let data = data, let response = response as? HTTPURLResponse {
@@ -28,6 +36,8 @@ public class URLSessionHTTPClient: HTTPClient {
                     throw UnexpectedValueRepresentation()
                 }
             })
-        }.resume()
+        }
+        task.resume()
+        return URLSessionTaskWrapper(wrapped: task)
     }
 }
--- CleanNewsFrameworkTests/News API/RemoteNewsLoaderTests.swift
+++ CleanNewsFrameworkTests/News API/RemoteNewsLoaderTests.swift
@@ -186,14 +186,19 @@ final class RemoteNewsLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
+        private struct Task: HTTPClientTask {
+            func cancel() {}
+        }
+        
         var requestedURLs: [URL] {
             messages.map { $0.url }
         }
         
         private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
         
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
             messages.append((url, completion))
+            return Task()
         }
         
         func complete(with error: Error, at index: Int = 0) {
