3e29736a090d066902bd2753b4827899448749a9
Fernando Putallaz <ferhedio@gmail.com>
2023-04-04 08:28:47 -0300

    Refactor `switch` statement with `map` and `flatMap` chain.

--- CleanNewsFramework/News API/RemoteFeedImageDataLoader.swift
+++ CleanNewsFramework/News API/RemoteFeedImageDataLoader.swift
@@ -47,18 +47,14 @@ public class RemoteFeedImageDataLoader: FeedImageDataLoader {
         let task = HTTPClientTaskWrapper(completion)
         task.wrapped = client.get(from: url) { [weak self] result in
             guard self != nil else { return }
-            
-            switch result {
-            case let .success((data, response)):
-                if response.statusCode == 200, !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
+        
+            task.complete(with: result
+                .mapError { _ in Error.connectivity }
+                .flatMap { (data, response) in
+                    let isValidResponse = response.statusCode == 200 && !data.isEmpty
+                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
                 }
-                
-            case .failure:
-                task.complete(with: .failure(Error.connectivity))
-            }
+            )
         }
         return task
     }
