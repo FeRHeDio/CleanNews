94a73e4a2451e0a46bced50bb151289eafa311cd
Fernando Putallaz <ferhedio@gmail.com>
2023-04-04 16:46:12 -0300

    Add `LocalFeedImageDataLoaderTests` file.

---
+++ CleanNewsFrameworkTests/News Cache/LocalFeedImageDataLoaderTests.swift
@@ -0,0 +1,15 @@
+//
+//  LocalFeedImageDataLoaderTests.swift
+//  CleanNewsFrameworkTests
+//
+//  Created by Fernando Putallaz on 04/04/2023.
+//
+
+import XCTest
+import CleanNewsFramework
+
+
+class LocalFeedImageDataLoaderTests: XCTestCase {
+    
+    
+}
