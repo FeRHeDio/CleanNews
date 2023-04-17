51fc72c77d922c3bb1cfd6877403504146796d0b
Fernando Putallaz <ferhedio@gmail.com>
2023-04-17 16:16:08 -0300

    Refactor procedural code into a chain of `map` operations

--- CleanNewsFramework/News Cache/CoreDataNewsStore+FeedImageDataLoader.swift
+++ CleanNewsFramework/News Cache/CoreDataNewsStore+FeedImageDataLoader.swift
@@ -11,9 +11,9 @@ extension CoreDataNewsStore: FeedImageDataStore {
     public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
         perform { context in
             completion(Result {
-                let image = try ManagedNewsItem.first(with: url, in: context)
-                image?.data = data
-                try context.save()
+                try ManagedNewsItem.first(with: url, in: context)
+                    .map { $0.data = data }
+                    .map(context.save)
             })
         }
     }
